f8eb79c56422f7a5ebaf406cd5fd361c4f99755b
Niraj Kumar Chauhan <nirajkumar.chauhan@pearson.com>
2017-01-15 22:48:03 +0530

    Changes and bug fixes

--- app/src/main/java/com/meapp/anzme/MainActivity.java
+++ app/src/main/java/com/meapp/anzme/MainActivity.java
@@ -3,6 +3,10 @@ package com.meapp.anzme;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 //Neura import
 import com.neura.standalonesdk.service.NeuraApiClient;
@@ -16,13 +20,42 @@ public class MainActivity extends AppCompatActivity {
     private NeuraApiClient mNeuraApiClient;
     private String NEURA_APP_UID = "ba13394b9ff69d5880b6bf13fcffccd3c642f4d5515967ec3156de20f7e13f54";
     private String NEURA_APP_SECRET = "8b1dac7a7aba06b14f182b28c2680f7b877d8f87ca7527cc7fe105a082334256";
+    private WebView clientWebview;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clientWebview = (WebView) findViewById(R.id.clientWebview);
+        clientWebview.loadUrl("https://www.finup.co/c2/sigma/mobile/");
+        WebSettings clientWebSetting = clientWebview.getSettings();
+        clientWebSetting.setBuiltInZoomControls(true);
+        clientWebSetting.setJavaScriptEnabled(true);
+        clientWebview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+        });
         init();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (clientWebview.canGoBack()) {
+                        clientWebview.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
     //Neura CLient initialistion
